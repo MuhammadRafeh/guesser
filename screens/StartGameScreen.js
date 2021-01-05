361697bddba86ebe52b8e3e59f9282c5e176ddca
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-05 17:46:00 +0300

    Added Start Game Screen Component

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+
+        </View>
+    );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    }
+});
