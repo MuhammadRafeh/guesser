a3e9b6cfe8487bd0b3e72ba33afddba48020e8f2
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-06 07:43:44 +0300

    Added Game Screen

---
+++ screens/GameScreen.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const generateRandomNumber = ( min, max, exclude ) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    if (rndNum === exclude) {
+        return generateRandomNumber(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+}
+
+const GameScreen = props => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, props.userChoice))
+    return (
+        
+    );
+}
+
+export default GameScreen;
+    
+const styles = StyleSheet.create({
+
+});
