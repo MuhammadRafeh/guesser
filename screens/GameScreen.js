248cf79db1dfb8df343329cdefda1dc0351c30dc
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-06 10:04:37 +0300

    Navigating between Screens

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import Card from '../components/Card';
+import NumberContainer from '../components/NumberContainer';
 
 const generateRandomNumber = ( min, max, exclude ) => {
     min = Math.ceil(min);
@@ -13,14 +15,34 @@ const generateRandomNumber = ( min, max, exclude ) => {
 }
 
 const GameScreen = props => {
-    const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, props.userChoice))
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomNumber(1, 100, props.userChoice)
+    );
     return (
-        
+       <View style={styles.screen}>
+           <Text>Opponent's Guess</Text>
+           <NumberContainer>{currentGuess}</NumberContainer>
+           <Card style={styles.buttonContainer}>
+               <Button title="Lower" onPress={() => {}}/>
+               <Button title="Greater" onPress={() => {}}/>
+           </Card>
+       </View> 
     );
 }
 
 export default GameScreen;
     
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
 });
