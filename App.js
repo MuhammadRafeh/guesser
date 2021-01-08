3eb9f5ae04c0c2f513fd2255765f016698b97e3b
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-08 17:48:22 +0300

    Fonts Added

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-// import { useFonts } from "@use-expo/font";
-import {useFonts} from 'expo-font';
+import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading'
 
 import Header from './components/Header';
@@ -17,7 +16,8 @@ function App() {
   const [rounds, setRounds] = useState(0)
 
   let [fontsLoaded] = useFonts({
-    'Inter-Black': require('./assets/fonts/ OpenSans-Regular.tff')
+    'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
 
   if (!fontsLoaded) {
