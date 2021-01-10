b2e8db25b2129235ca6850f6c225d5130b7cccee
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-10 21:33:44 +0300

    Split MainButton file into 2 os based files

--- components/MainButton.ios.js
+++ components/MainButton.ios.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import colors from '../constants/colors';
 
 const MainButton = props => {
-
-    let ButtonComponent = TouchableOpacity
-
-    if (Platform.OS === 'android' && Platform.Version >= 21) {
-        ButtonComponent = TouchableNativeFeedback
-    }
-
     return (
         <View style={styles.buttonContainer}>
-            <ButtonComponent activeOpacity={0.2} onPress={props.onPress}>
+            <TouchableOpacity activeOpacity={0.2} onPress={props.onPress}>
                 <View style={styles.button}>
                     <Text style={styles.buttonText}>{props.children}</Text>
                 </View>
-            </ButtonComponent>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -24,10 +17,6 @@ const MainButton = props => {
 export default MainButton;
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        borderRadius: 25,
-        overflow: 'hidden'
-    },
     button: {
         backgroundColor: colors.primary,
         paddingVertical: 12,
